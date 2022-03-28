6ddb923b780ba310cd133a7b6f7dfe8f0e7dd07b
CodeMe1fYouCan <102505412+CodeMe1fYouCan@users.noreply.github.com>
2022-03-28 00:25:26 -0700

    Fix script to catch delayed element loads
    The elements we need to delete don't always load with the page. Setting a short 3 second check to find and delete the elements. If it takes longer than 3 seconds for the elements to load, then YouTube is having a terrible, horrible, no good, very bad day.

--- youtube-suggested-video-remover.user.js
+++ youtube-suggested-video-remover.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  The suggested videos at the end of Youtube videos block the screen before the current video is even over. I find this annoying, so I wrote this script to remove them.
-// @author       CodeMe1fYouCan
+// @author       CodeMeIfYouCan
 // @match        https://www.youtube.com/watch*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
@@ -11,8 +11,37 @@
 
 (function() {
     'use strict';
-    window.addEventListener('load', function() {
-        const removeElements = (elms) => elms.forEach(el => el.remove());
-        removeElements(document.querySelectorAll(".ytp-ce-element"));
-    }, false);
+  main();
 })();
+
+async function main() {
+  await waitForYTVideoElementsToLoad();
+  removeYTVideoElements();
+}
+
+// Wait for the ytb-ce elements we want to delete to load
+async function waitForYTVideoElementsToLoad() {
+  var numWaits = 0;
+  var elements = document.querySelectorAll(".ytp-ce-element");
+  while(elements == null || !elements.length) {
+    // Only wait 3 seconds. There may not be any elements to delete in the first place.
+    if (numWaits >= 3) {
+      break;
+    }
+
+    await new Promise(r => setTimeout(r, 1000));
+    elements = document.querySelectorAll(".ytp-ce-element");
+    numWaits++;
+  }
+}
+
+function removeYTVideoElements() {
+  const removeElements = (elms) => elms.forEach(el => el.remove());
+  let elements = document.querySelectorAll(".ytp-ce-element");
+  // Uncomment if you want to see what elements are being removed in the developer console
+  /*console.log(elements.length);
+  for (var i = 0, element; (element = elements[i]); i++) {
+    console.log(element);
+  }*/
+  removeElements(elements);
+}
